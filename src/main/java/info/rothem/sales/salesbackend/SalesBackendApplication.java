4810ba3e8803b6abd22a1806347f6a1619675dca
Nakya Santini <nakya.santini@tes.co.id>
2018-11-18 12:53:58 +0700

    Init Template

---
+++ src/main/java/info/rothem/sales/salesbackend/SalesBackendApplication.java
@@ -0,0 +1,12 @@
+package info.rothem.sales.salesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/info/rothem/sales/salesbackend/SalesBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package info.rothem.sales.salesbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SalesBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
