0765a462464e065d673ce9dc24a5e0e3d62913ac
Nakya Santini <nakya.santini@tes.co.id>
2018-11-26 15:41:57 +0700

    Added MySQL Databases

--- src/main/java/info/rothem/sales/salesbackend/controller/LoginController.java
+++ src/main/java/info/rothem/sales/salesbackend/controller/LoginController.java
@@ -1,8 +1,15 @@
 package info.rothem.sales.salesbackend.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,17 +18,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
+    @Autowired
+    private DataSource datasource;
+
     @CrossOrigin(origins = "http://localhost:7000")
     @RequestMapping("/login")
-    public Map<String, String> auth(@RequestParam String username, @RequestParam String password) {
-        final String usernameToken = "hello";
-        final String passwordToken = "there";
+    public Map<String, Integer> auth(@RequestParam String username, @RequestParam String password) {
+        Map<String, Integer> data = new LinkedHashMap<>();
 
-        Map<String, String> data = new LinkedHashMap<>();
-        
-        if(username.equals(usernameToken) && password.equals(passwordToken)) data.put("Status", "1");
-            else data.put("Status", "0");
+        try {
+            Connection con = datasource.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM user WHERE username = ?");
+                ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
 
-        return data;
+            if(rs.next()) {
+                if(password.equals(rs.getString("password"))) {
+                    data.put("status", 1);
+                } else {
+                    data.put("status", 0);
+                }
+            } else {
+                data.put("status", 0);
+            }
+
+            rs.close();
+            ps.close();
+            con.close();
+        }catch(SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
+        return data; 
     }
 }
